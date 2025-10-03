970f462ab97697b3267b8e13f044765c62fd5162
Isemiyake <t.semih.pro@gmail.com>
2025-10-03 11:06:22 +0200

    Structure MVC initiale

---
+++ app/config/params.php
@@ -0,0 +1,10 @@
+<?php
+// Params
+
+// Parametres de connexion a la DB
+define('DB_HOST','localhost:8889');
+define('DB_NAME','alex_parker_octobre_2021');
+define('DB_USER','root');
+define('DB_PASSWORD','root');
+
+
---
+++ core/init.php
@@ -0,0 +1,7 @@
+<?php
+// Initialisation
+
+// 1. Charger les params
+require_once '../app/config/params.php';
+// 2. Charger la connexion
+require_once '../core/connexion.php';
